ba6463aa8df221f967cad427cd625d4d108768bf
zeertag <taras0705@yandex.ru>
2025-07-03 21:39:15 +0300

    Добавьте файлы проекта.

---
+++ prac 3/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace prac_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 453);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(641, 51);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(493, 453);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(285, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Фрактал";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(861, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Дерево";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(128, 520);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Нарисовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(780, 520);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(210, 33);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Нарисовать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(366, 526);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(37, 20);
+            this.numericUpDown1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(409, 528);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Глубина";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(1150, 582);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ prac 3/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prac_3
+{
+    public partial class Form1 : Form
+    {
+        private List<(int X, int Y, int Depth)> nodes = new List<(int X, int Y, int Depth)>();
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox2.MouseClick += pictureBox2_MouseClick;
+        }
+
+        private void DrawTree(Graphics g, int x, int y, int angle, int length, int n, int stop)
+        {
+            var state = g.Save();
+            g.TranslateTransform(x, y);
+            g.RotateTransform(angle);
+
+            int r = Math.Abs((255 - n * 30) % 256);
+            int gr = Math.Abs((255 - n * 80) % 256);
+            int b = Math.Abs((255 - n * 80) % 256);
+
+            //рисуем большой квадрат
+            g.FillRectangle(new SolidBrush(Color.FromArgb(r, gr, b)), -length / 2, -length, length, length);
+
+            //рисуем 2 малых
+            int nextX1 = (int)(-length / 4);
+            int nextX2 = (int)(length / 4);
+            int nextY = (int)(-length * 5 / 4);
+
+            length = (int)(length / 1.4f);
+
+
+            //1
+            var state1 = g.Save();
+            g.TranslateTransform(nextX1, nextY);
+            g.RotateTransform(-45);
+            g.FillRectangle(new SolidBrush(Color.FromArgb(r, gr, b)), -length / 2, -length, length, length);
+            g.Restore(state1);
+
+            //2
+            var state2 = g.Save();
+            g.TranslateTransform(nextX2, nextY);
+            g.RotateTransform(45);
+            g.FillRectangle(new SolidBrush(Color.FromArgb(r, gr, b)), -length / 2, -length, length, length);
+            g.Restore(state2);
+
+
+
+            if (n < stop)
+            {
+                int newX1 = (int)(-length / 4);
+                int newX2 = (int)(length / 4);
+                int newX3 = (int)(-length / 4);
+                int newX4 = (int)(length / 4);
+                int newY = (int)(-length * 5 / 4);
+
+                var state3 = g.Save();
+                g.TranslateTransform(nextX2, nextY);
+                g.RotateTransform(45);
+
+                DrawTree(g, newX1, newY, -45, (int)(length / 1.4f), n + 1, stop);
+                DrawTree(g, newX2, newY, 45, (int)(length / 1.4f), n + 1, stop);
+                g.Restore(state3);
+
+                var state4 = g.Save();
+                g.TranslateTransform(nextX1, nextY);
+                g.RotateTransform(-45);
+                DrawTree(g, newX3, newY, -45, (int)(length / 1.4f), n + 1, stop);
+                DrawTree(g, newX4, newY, 45, (int)(length / 1.4f), n + 1, stop);
+                g.Restore(state4);
+            }
+
+            g.Restore(state);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            double[] coefs = { 0.2, 0.5, 0.8 };
+
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White);
+
+                for (int i = 1; i < numericUpDown1.Value + 1; i++)
+                {
+                    if (i < 4)
+                        DrawTree(g, (int)(pictureBox1.Width * coefs[(i - 1) % 3]), pictureBox1.Height / 2 - 10, 0, 35, 1, i);
+                    else
+                        DrawTree(g, (int)(pictureBox1.Width * coefs[(i - 1) % 3]), pictureBox1.Height - 50, 0, 35, 1, i);
+                }
+            }
+
+            pictureBox1.Image = bmp;
+        }
+
+
+        public class TreeNodeInfo
+        {
+            public int X, Y;
+            public int Depth;
+        }
+
+        private void DrawScelet(Graphics g, int x, int y, int n, int stop, int totalWidth)
+        {
+            int dy = 60;
+            int radius = 6;
+
+            nodes.Add((x, y, n));
+
+            g.FillEllipse(Brushes.LightGreen, x - radius, y - radius, 2 * radius, 2 * radius);
+            g.DrawEllipse(Pens.Black, x - radius, y - radius, 2 * radius, 2 * radius);
+
+            if (n >= stop - 1) return;
+
+            int numChildren = 4;
+            int level = n + 1;
+            int levelNodes = (int)Math.Pow(numChildren, level);
+
+            int spacing = totalWidth / levelNodes;
+
+            int baseX = x - ((spacing * (numChildren - 1)) / 2);
+            int childY = y + dy;
+
+            for (int i = 0; i < numChildren; i++)
+            {
+                int childX = baseX + i * spacing;
+
+                g.DrawLine(Pens.Black, x, y + radius, childX, childY - radius);
+
+                DrawScelet(g, childX, childY, n + 1, stop, totalWidth);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                g.Clear(Color.White);
+
+                int depth = 6;
+                int margin = 20;
+                int totalWidth = pictureBox2.Width - 2 * margin;
+
+                nodes.Clear();
+                DrawScelet(g, pictureBox2.Width / 2, 30, 0, depth, totalWidth);
+            }
+
+            pictureBox2.Image = bmp;
+        }
+
+        private void pictureBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            int radius = 6;
+            foreach (var node in nodes)
+            {
+                int dx = e.X - node.X;
+                int dy = e.Y - node.Y;
+                if (dx * dx + dy * dy <= radius * radius)
+                {
+                    MessageBox.Show($"Уровень узла: {node.Depth + 1}");
+                    numericUpDown1.Value = node.Depth + 1;
+                    return;
+                }
+            }
+        }
+    }
+}
